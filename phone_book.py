f069caecc0f113e74ee232894c6f92554bba089e
rababe azil <azilrababe@gmail.com>
2023-01-18 11:28:52 +0100

    ...

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,8 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csvfile:
+    csv_reader = csv.DictReader(csvfile)
+    line_count = 0
+    for row in csv_reader:
+        print(row['last_name'],' : ',int(row['phone_number']))
+        line_count += 1
